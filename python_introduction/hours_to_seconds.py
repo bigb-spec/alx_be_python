e9483ce42c61f901eb5d83a31553f47db36a9420
BigBrandon <archbrandonkiprotich@gmail.com>
2025-07-10 08:45:45 +0300

    hours to seconds

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,7 +1,4 @@
 hours = 2
-minutes_in_an_hour = 60
-seconds_in_a_minute = 60
-seconds_in_an_hour = minutes_in_an_hour * seconds_in_a_minute
-seconds_in_2_hours = seconds_in_an_hour * 2
+seconds = 2 * 3600
 
-print("2 hour(s) is", seconds_in_2_hours, 'seconds.')
+print("2 hour(s) is", seconds, "seconds.")
