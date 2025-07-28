b93d539ab84065377a6cf01dce4a324e8ac3c362
BigBrandon <archbrandonkiprotich@gmail.com>
2025-07-28 12:39:20 +0300

    Arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,7 +3,7 @@ num1 = float(input("Enter the first number:"))
 num2 = float(input("Enter the second number:"))
 operation = input("Enter the operation (add, subtract, multiply, divide): ")
 
-def  perform_operation(num1: float, num2: float, operation: str):
+def  perform_operation(num1, num2, operation):
     """Performs basic arithmetic operations on two numbers.
     
     Args:
